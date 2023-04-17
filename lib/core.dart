e1631371e903200918da81e4df4a72ed4cce916b
caspertrack69 <ar886069@gmail.com>
2023-04-17 08:27:49 +0700

    update package

---
+++ lib/core.dart
@@ -0,0 +1,8 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:uikit/module/dashboard/controller/dashboard_controller.dart';
+export 'package:uikit/module/dashboard/view/dashboard_view.dart';
+export 'state_util.dart';
